0edb5ab12fdf20c23ac119341daeb99dff3d72db
LP <devmaterials@gmail.com>
2016-06-19 17:27:48 +0200

    Create project

---
+++ Invoice v1.0/Models/DaneDoFaktury.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Invoice_v1._0.Models
+{
+    public class DaneDoFaktury
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Gender { get; set; }
+        public string City { get; set; }
+        public string Product { get; set; }
+        public string Amounts { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ Invoice v1.0/Models/DaneDoFakturyContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Invoice_v1._0.Models
+{
+    public class DaneDoFakturyContext : DbContext
+    {
+        public DbSet<DaneDoFaktury> DaneDoFaktury { get; set; }
+    }
+}
